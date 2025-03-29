aa8489d45d315b6bf8dd16e4e64249c4d270c218
José Ulisses <julissesmacedo@outlook.com>
2025-03-28 23:36:23 -0300

    Novos metodos para API

--- lib/src/core/api/shortener.api.dart
+++ lib/src/core/api/shortener.api.dart
@@ -1,15 +1,17 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:shortenerapp/src/core/models/auth/login.request.dart';
+import 'package:shortenerapp/src/core/models/auth/login.response.dart';
 import 'package:shortenerapp/src/core/models/url_shortener/create-url-shortener.request.dart';
 import 'package:shortenerapp/src/core/models/url_shortener/url_shortener.response.dart';
 
 class ShortenerApi {
-  static const url = 'http://localhost:3000/';
+  static const url = 'http://localhost:3000';
 
-  Future<UrlShortenerResponse> post(CreateUrlShortenerRequest request) async {
+  Future<LoginResponse> login(LoginRequest request) async {
     var client = http.Client();
-    var uri = Uri.parse('$url/');
+    var uri = Uri.parse('$url/auth');
     var response = await client.post(
       uri,
       headers: <String, String>{
@@ -18,8 +20,50 @@ class ShortenerApi {
       body: jsonEncode(request.toJson()),
     );
 
+    if (response.statusCode == 200) {
+      return LoginResponse.fromRawJson(response.body);
+    } else {
+      throw Exception();
+    }
+  }
+
+  Future<UrlShortenerResponse> post(
+    CreateUrlShortenerRequest request,
+    String? token,
+  ) async {
+    var client = http.Client();
+    var uri = Uri.parse('$url/');
+    var response = await client.post(
+      uri,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+      body: jsonEncode(request.toJson()),
+    );
+
     if (response.statusCode == 201) {
       return UrlShortenerResponse.fromRawJson(response.body);
+    } else {
+      throw Exception();
+    }
+  }
+
+  Future<List<UrlShortenerResponse>> getAllUrl(String token) async {
+    var client = http.Client();
+    var uri = Uri.parse('$url/all');
+    var response = await client.get(
+      uri,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return List<UrlShortenerResponse>.from(
+        json.decode(response.body).map((x) => UrlShortenerResponse.fromJson(x)),
+      );
     } else {
       throw Exception();
     }
