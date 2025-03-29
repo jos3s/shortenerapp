a91e6541e9128327e691bf5eb22d42acf84ae7e8
José Ulisses <julissesmacedo@outlook.com>
2025-03-28 23:38:37 -0300

    Salvando token do login no estado do app

--- lib/src/shared/app_state.dart
+++ lib/src/shared/app_state.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:shortenerapp/src/core/models/url_shortener/url_shortener.response.dart';
 
 class AppState extends ChangeNotifier {
-  var urlShorteners = [
-  ];
+  String? accessToken;
+
+  var urlShorteners = [];
 
   addNewUrl(UrlShortenerResponse response) {
     urlShorteners.add(response);
@@ -14,4 +15,7 @@ class AppState extends ChangeNotifier {
     urlShorteners.removeWhere((url) => url.id == id);
     notifyListeners();
   }
+
+  saveToken(String token) => accessToken = token;
+  clearToken() => accessToken = null;
 }
