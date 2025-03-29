9d38b1f41d23c5dab6002c9460db2d5b3afaa117
José Ulisses <julissesmacedo@outlook.com>
2025-03-29 12:38:30 -0300

    Criando SecureStorage

---
+++ lib/src/core/storage/secure_storage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  final storage = const FlutterSecureStorage();
+
+  final String _keyEmail = "email";
+  final String _keyPassword = "password";
+  final String _keyToken = "token";
+
+   Future setEmail(String username) async {
+    await storage.write(key: _keyEmail, value: username);
+  }
+
+  Future<String?> getEmail() async {
+    return await storage.read(key: _keyEmail);
+  }
+
+  Future setPassword(String password) async {
+    await storage.write(key: _keyPassword, value: password);
+  }
+
+  Future<String?> getPassword() async {
+    return await storage.read(key: _keyPassword);
+  }
+
+  Future setToken(String token) async {
+    await storage.write(key: _keyToken, value: token);
+  }
+
+  Future<String?> getToken() async {
+    return await storage.read(key: _keyToken);
+  }
+}
