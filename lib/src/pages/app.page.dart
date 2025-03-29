585a5ea3c1c14b1043599a6d1031ae8cf4e94d7a
José Ulisses <julissesmacedo@outlook.com>
2025-03-28 23:37:14 -0300

    Movendo lógica da página home para seu próprio arquivo

--- lib/src/pages/app.page.dart
+++ lib/src/pages/app.page.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shortenerapp/src/core/models/url_shortener/url_shortener.response.dart';
 import 'package:shortenerapp/src/forms/shortener.form.dart';
+import 'package:shortenerapp/src/pages/historic/historic.page.dart';
+import 'package:shortenerapp/src/pages/home/home.page.dart';
 import 'package:shortenerapp/src/shared/app_state.dart';
+import 'package:shortenerapp/src/shared/pages.enum.dart';
+import 'package:shortenerapp/src/widgets/account.widget.dart';
 import 'package:shortenerapp/src/widgets/url_card.widget.dart';
 
 class AppPage extends StatefulWidget {
@@ -15,34 +19,44 @@ class AppPage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<AppPage> {
+  Pages selectedPage = Pages.home;
+
   @override
   Widget build(BuildContext context) {
     var state = context.watch<AppState>();
 
+    
+    Widget page;
+    switch (selectedPage) {
+      case Pages.home:
+        page = HomePage();
+        break;
+      case Pages.historic:
+        page = HistoricPage();
+        break;
+    }
+
+
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ShortenerForm(),
-                SizedBox(height: 20),
-                for (UrlShortenerResponse urlResponse in state.urlShorteners)
-                  UrlCardWidget(
-                    id: urlResponse.id,
-                    url: urlResponse.url,
-                    originalUrl: urlResponse.originalUrl,
-                    creationAt: urlResponse.createdAt,
-                    updatedAt: urlResponse.updatedAt,
-                  ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      appBar: AppBar(title: Text(widget.title), actions: [AccountWidget()]),
+      body: page,
+      bottomNavigationBar: _navigationBar(),
+    );
+  }
+
+  NavigationBar _navigationBar() {
+    return NavigationBar(
+      destinations: [
+        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+        //NavigationDestination(icon: Icon(Icons.history), label: 'History'),
+        NavigationDestination(icon: Icon(Icons.history), label: 'Historic'),
+      ],
+      onDestinationSelected: (value) {
+        setState(() {
+          selectedPage = Pages.values[value];
+        });
+      },
+      selectedIndex: selectedPage.index,
     );
   }
 }
---
+++ lib/src/pages/home/home.page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shortenerapp/src/core/models/url_shortener/url_shortener.response.dart';
+import 'package:shortenerapp/src/forms/shortener.form.dart';
+import 'package:shortenerapp/src/shared/app_state.dart';
+import 'package:shortenerapp/src/widgets/url_card.widget.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var state = context.watch<AppState>();
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ShortenerForm(),
+              SizedBox(height: 20),
+              for (UrlShortenerResponse urlResponse in state.urlShorteners)
+                UrlCardWidget(
+                  id: urlResponse.id,
+                  url: urlResponse.url,
+                  originalUrl: urlResponse.originalUrl,
+                  creationAt: urlResponse.createdAt,
+                  updatedAt: urlResponse.updatedAt,
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
